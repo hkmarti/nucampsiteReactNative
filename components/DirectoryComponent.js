c1383ed57539cd8592e9ea5f34ceb116284c1eaf
Heather Martin <hkoto328@gmail.com>
2021-09-27 18:30:52 -0400

    Components Part 2

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -5,10 +5,18 @@ import { ListItem } from 'react-native-elements';
 function Directory(props) {
 
     const renderDirectoryItem = ({item}) => {
+
+        //ListItem onPress -> when item is pressed, function will automatically fire //
+
+        //This ListItem onPress triggers the onCampsiteSelect event handler from MainComponent that was passed via props.//
+
+        //The id of the pressed campsite will update item.id and give that value to the onCampsiteSelect event handler. That will then update the selectedCampsite property in the MainComponent with this id.//
+
         return (
             <ListItem
                 title={item.name}
                 subtitle={item.description}
+                onPress = {() => props.onPress(item.id)}
                 leftAvatar={{ source: require('./images/react-lake.jpg')}}
             />
         );
