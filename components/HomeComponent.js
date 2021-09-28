626a68d7e3bc5fe9d1d9d367d685fba1c27a40ae
Heather Martin <hkoto328@gmail.com>
2021-09-28 15:33:29 -0400

    Navigation Part 3

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,7 +1,39 @@
 import React, { Component } from "react";
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from "react-native-elements";
+import { CAMPSITES } from '../shared/campsites';
+import { PROMOTIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
+
+function RenderItem({item}) {
+    if (item){
+        return (
+            //Styles the Card//
+            <Card
+                featuredTitle={item.name}
+                image={require('./images/react-lake.jpg')}>
+                <Text 
+                    style={{margin:10}}>
+                    {item.description}    
+                </Text>
+            </Card>
+        );
+    } else {
+        return <View />;
+    }
+}
+
 
 class Home extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            campsites: CAMPSITES,
+            promotions: PROMOTIONS,
+            partners: PARTNERS,
+        };
+    }
 
     //Title of Header
     static navigationOptions = {
@@ -10,9 +42,15 @@ class Home extends Component {
 
     render() {
         return (
-            <View>
-                <Text>Home Component</Text>
-            </View>
+            //Renders Cards for Campsites, Promotions and Partners//
+            <ScrollView>
+                <RenderItem
+                    item={this.state.campsites.filter(campsite => campsite.featured)[0]} />
+                <RenderItem
+                    item={this.state.promotions.filter(promotion => promotion.featured)[0]} />
+                <RenderItem
+                    item={this.state.partners.filter(partner => partner.featured)[0]} />    
+            </ScrollView>
         );
     }
 }
