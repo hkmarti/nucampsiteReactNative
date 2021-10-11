eb3424452f3b45167a47e0ebd366bac72c02d103
Heather Martin <hkoto328@gmail.com>
2021-10-11 14:38:58 -0400

    Swipe and Delete Favorites

--- redux/favorites.js
+++ redux/favorites.js
@@ -7,10 +7,13 @@ export const favorites = (state = [], action) => {
             if (state.includes(action.payload)){
                 return state;
             //If campsite.id is NOT included in favorites array, then return state with changes using concat. (aka. make copy of array with new changes included without mutating the original array).//
-            } else {
-                return state.concat(action.payload);
-            } 
-            default: 
-                return state;
+            } //else//
+            return state.concat(action.payload);
+            
+        case ActionTypes.DELETE_FAVORITE:
+            return state.filter(favorite => favorite !== action.payload);
+
+        default: 
+            return state;
     }
 }
